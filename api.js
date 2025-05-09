56c05d030b2bf53a520712fe13ec812d7a2cb0bc
Fahima93-cloud <fahima.practice03@gmail.com>
2025-05-09 14:09:53 +0600

    eight commit

--- api.js
+++ api.js
@@ -1,8 +1,6 @@
 async function searchCountry() {
   const countryName = document.getElementById('countryInput').value.trim();
   const result = document.getElementById('result');
-  
-  result.innerHTML = ""; // Clear previous results
 
   if (countryName === "") {
     alert("Please enter a country name!");
